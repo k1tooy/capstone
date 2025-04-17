15f3600d7335097835097dde99d87d1afb001095
k1tooy <108557464+k1tooy@users.noreply.github.com>
2025-04-17 16:51:27 +0800

    add captureImage function

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-from gpiozero import LED
-from time import sleep
 import random
+from gpiozero import LED
+import time
+from time import sleep
+import os
+from picamera2 import Picamera2
 
 # Mapping class labels to GPIO pins, excluding "empty"
 signal_pins = {"abnoy": 17, "fertilized": 27, "unfertilized": 22}
 
 # Prepare LED objects
 signals = {label: LED(pin) for label, pin in signal_pins.items()}
+
+
+picam2 = Picamera2()
+picam2.start()
 
 
 def sendSignal(label: str):
@@ -17,22 +24,42 @@ def sendSignal(label: str):
         led.off()
 
 
-def getImage():
+def captureImage(output_dir="data", subfolder="default"):
+    path = os.path.join(output_dir, subfolder)
+    os.makedirs(path, exist_ok=True)
+    timestamp = time.strftime("%Y%m%d_%H%M%S")
+    file_path = os.path.join(path, f"image_{timestamp}.jpg")
+    picam2.capture_file(file_path)
+    print(f"Image captured to {file_path}")
+    return file_path
+
+
+def moveImage():
     pass
 
 
-def identifyEgg():
+def deleteImage():
+    pass
+
+
+def identifyEgg(image):
     return random.choice(["abnoy", "empty", "fertilized", "unfertilized"])
+
+def isFinished():
 
 
 def main():
     class_names = ["abnoy", "empty", "fertilized", "unfertilized"]
 
     while True:
-        label = identifyEgg()
+        image_path = captureImage(subfolder="run1")
+        label = identifyEgg(image_path)
         print(f"Identified: {label}")
         if label != "empty":
             sendSignal(label)
+            moveImage()
+        else:
+            deleteImage()
         sleep(1)
 
 
